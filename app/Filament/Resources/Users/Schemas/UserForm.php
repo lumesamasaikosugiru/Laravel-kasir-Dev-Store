57b960cb957b9807d56d241b20de97051717baa2
lumesamasaikosugiru <lumetrixsynf@gmail.com>
2025-09-23 11:12:09 +0700

    wasoi

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Illuminate\Support\Facades\Hash;
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->required(),
+                TextInput::make('password')
+                    ->password()
+                    ->dehydrateStateUsing(fn(string $state): string => Hash::make($state))
+                    ->dehydrated(fn(?string $state): bool => filled($state))
+            ]);
+    }
+}
