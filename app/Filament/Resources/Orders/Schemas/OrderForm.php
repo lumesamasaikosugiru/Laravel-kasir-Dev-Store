cc0fb7656ca2d1b8404a9f695db2fdd5167941a3
lumesamasaikosugiru <lumetrixsynf@gmail.com>
2025-09-24 22:46:56 +0700

    Customize form order

--- app/Filament/Resources/Orders/Schemas/OrderForm.php
+++ app/Filament/Resources/Orders/Schemas/OrderForm.php
@@ -2,11 +2,16 @@
 
 namespace App\Filament\Resources\Orders\Schemas;
 
-use Filament\Forms\Components\DatePicker;
+use App\Models\Customer;
+use App\Models\OrderDetail;
+use App\Models\Product;
 use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Utilities\Get;
+use Filament\Schemas\Components\Utilities\Set;
 use Filament\Schemas\Schema;
 
 class OrderForm
@@ -15,21 +20,84 @@ class OrderForm
     {
         return $schema
             ->components([
+                DateTimePicker::make('date_sell')
+                    ->required()
+                    ->default(now())
+                    ->disabled()
+                    ->hiddenLabel()
+                    ->dehydrated()
+                    ->prefix('Date:'),
+
                 Section::make('Customer Info')
                     ->description('Data terkait pelanggan dan penjualan')
                     ->schema([
                         Select::make('customer_id')
                             ->relationship('customer', 'name')
-                            ->required(),
+                            ->required()
+                            ->reactive()
+                            ->afterStateUpdated(function ($state, Set $set) {
+                                $customer = Customer::find($state);
+                                $set('phone', $customer->phone ?? null);
+                                $set('address', $customer->address ?? null);
+                            }),
+
+                        TextInput::make('phone')
+                            ->disabled(),
+                        TextInput::make('address')
+                            ->disabled(),
                     ])
+                    ->columns(3)
                     ->columnSpanFull(),
-                DateTimePicker::make('date_sell')
-                    ->required()
-                    ->default(now()),
+
+                Section::make('Detail Penjualan')
+                    ->description('Data Poduk terjuan')
+                    ->schema([
+                        Repeater::make('OrderDetail')
+                            ->relationship()
+                            ->schema([
+                                Select::make('product_id')
+                                    ->relationship('product', 'name')
+                                    ->reactive()
+                                    ->afterStateUpdated(function ($state, Set $set, Get $get) {
+                                        $product = Product::find($state);
+                                        $price = $product->price ?? 0;
+                                        $set('price', $price);
+                                        $qty = $get('qty') ?? 1;
+                                        $set('qty', $qty);
+                                        $subtotal = $price * $qty;
+                                        $set('subtotal', $subtotal);
+
+                                        $items = $get('../../OrderDetail') ?? [];
+                                        $total = collect($items)->sum(fn($item) => $item['subtotal'] ?? 0);
+                                        $set('../../total_price', $total);
+                                    }),
+
+                                TextInput::make('price')
+                                    ->disabled(),
+                                TextInput::make('qty')
+                                    ->numeric()
+                                    ->default(1)
+                                    ->reactive()
+                                    ->afterStateUpdated(function ($state, Set $set, Get $get) {
+                                        $price = $get('price') ?? 0;
+                                        $set('subtotal', $price * $state);
+
+                                        $items = $get('../../OrderDetail') ?? [];
+                                        $total = collect($items)->sum(fn($item) => $item['subtotal'] ?? 0);
+                                        $set('../../total_price', $total);
+                                    }),
+                                TextInput::make('subtotal')
+                                    ->disabled()
+                                    ->dehydrated(),
+                            ])->columns(4)
+                            ->columnSpanFull(),
+                    ])
+                    ->columns(3)
+                    ->columnSpanFull(),
 
                 TextInput::make('total_price')
                     ->required()
-                    ->numeric(),
+                    ->numeric()
             ]);
     }
 }
--- app/Filament/Resources/Orders/Tables/OrdersTable.php
+++ app/Filament/Resources/Orders/Tables/OrdersTable.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Orders\Tables;
 
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
@@ -39,6 +40,7 @@ class OrdersTable
             ->recordActions([
                 ViewAction::make(),
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
